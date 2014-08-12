0e628447dbe64f1cf1e8d3bb92e8dbadb2bb0aa5
Tom Daggett <tomd@pobox.com>
2014-08-12 16:03:55 -0700

    autogenerated managed object subclasses

---
+++ FailedBankCD/FailedBankCD/FailedBankDetails.m
@@ -0,0 +1,20 @@
+//
+//  FailedBankDetails.m
+//  FailedBankCD
+//
+//  Created by Tom Daggett on 2014-08-12.
+//  Copyright (c) 2014 Tom Daggett. All rights reserved.
+//
+
+#import "FailedBankDetails.h"
+#import "FailedBankInfo.h"
+
+
+@implementation FailedBankDetails
+
+@dynamic zip;
+@dynamic closeDate;
+@dynamic updateDate;
+@dynamic info;
+
+@end
---
+++ FailedBankCD/FailedBankCD/FailedBankInfo.m
@@ -0,0 +1,20 @@
+//
+//  FailedBankInfo.m
+//  FailedBankCD
+//
+//  Created by Tom Daggett on 2014-08-12.
+//  Copyright (c) 2014 Tom Daggett. All rights reserved.
+//
+
+#import "FailedBankInfo.h"
+#import "FailedBankDetails.h"
+
+
+@implementation FailedBankInfo
+
+@dynamic name;
+@dynamic city;
+@dynamic state;
+@dynamic details;
+
+@end
