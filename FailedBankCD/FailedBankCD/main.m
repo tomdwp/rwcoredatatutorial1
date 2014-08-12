75064835a180410953342bcb21fb781aefe224d6
Tom Daggett <tomd@pobox.com>
2014-08-12 14:10:19 -0700

    adding xcode project

---
+++ FailedBankCD/FailedBankCD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FailedBankCD
+//
+//  Created by Tom Daggett on 2014-08-12.
+//  Copyright (c) 2014 Tom Daggett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FBCDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FBCDAppDelegate class]));
+    }
+}
